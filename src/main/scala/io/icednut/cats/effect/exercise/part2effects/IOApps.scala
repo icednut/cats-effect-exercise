1d58460e0ab622f9858b6ce3f8a1db4183caf4e9
icednut <wglee21g@gmail.com>
2022-10-10 09:00:02 +0900

    IOApp 연습

---
+++ src/main/scala/io/icednut/cats/effect/exercise/part2effects/IOApps.scala
@@ -0,0 +1,37 @@
+package io.icednut.cats.effect.exercise.part2effects
+
+import cats.effect.{ ExitCode, IO, IOApp }
+
+import scala.io.StdIn
+
+object IOApps {
+
+  val program = for {
+    line <- IO(StdIn.readLine())
+    _ <- IO(println(s"you've just written: $line"))
+  } yield ()
+}
+
+object TestApp {
+
+  import IOApps._
+
+  def main(args: Array[String]): Unit = {
+    import cats.effect.unsafe.implicits.global
+    program.unsafeRunSync()
+  }
+}
+
+object FirstCEApp extends IOApp {
+  import IOApps._
+
+  override def run(args: List[String]): IO[ExitCode] =
+    program.as(ExitCode.Success)
+}
+
+object MySimpleApp extends IOApp.Simple {
+
+  import IOApps._
+
+  override def run: IO[Unit] = program
+}
---
+++ src/main/scala/io/icednut/cats/effect/exercise/part2effects/IOErrorHandling.scala
@@ -0,0 +1,70 @@
+package io.icednut.cats.effect.exercise.part2effects
+
+import cats.effect.IO
+
+import scala.util.{ Failure, Success, Try }
+
+object IOErrorHandling {
+
+  // IO: pure, delay, defer
+  // create failed effects
+  val aFailedCompute: IO[Int] = IO.delay(throw new RuntimeException("A FAILURE"))
+  val aFailure: IO[Int]       = IO.raiseError(new RuntimeException("A PROPER FAILURE"))
+
+  // handle exceptions
+  val dealWithIt = aFailure.handleErrorWith { case _: RuntimeException =>
+    IO.delay(println("I'm still here."))
+  }
+
+  // turn into an Either
+  val effectAsEither: IO[Either[Throwable, Int]] = aFailure.attempt
+  // redeem: transform the failure and the success in one go
+  val resultAsString: IO[String] = aFailure.redeem(ex => s"FAIL: $ex", value => s"SUCCESS: $value")
+  val resultAsEffect: IO[Unit] =
+    aFailure.redeemWith(ex => IO.delay(println(s"FAIL: $ex")), value => IO.delay(println(s"SUCCESS: $value")))
+
+  /** Exercises
+    */
+  // 1 - construct potentially failed IOs from standard data types (Option, Try, Either)
+  def option2IO[A](option: Option[A])(ifEmpty: Throwable): IO[A] =
+//    option.map(v => IO.pure(v)).getOrElse(IO.raiseError(ifEmpty))
+    option match {
+      case Some(v) => IO(v)
+      case None    => IO.raiseError(ifEmpty)
+    }
+
+  def try2IO[A](aTry: Try[A]): IO[A] =
+//    aTry.fold(
+//      fa = ex => IO.raiseError(ex),
+//      fb = v => IO.pure(v)
+//    )
+    aTry match {
+      case Success(value)     => IO.pure(value)
+      case Failure(exception) => IO.raiseError(exception)
+    }
+
+  def either2IO[A](anEither: Either[Throwable, A]): IO[A] =
+//    anEither.fold(
+//      fa = ex => IO.raiseError(ex),
+//      fb = v => IO.pure(v)
+//    )
+    anEither match {
+      case Right(v) => IO.pure(v)
+      case Left(ex) => IO.raiseError(ex)
+    }
+
+  // 2 - handleError, handleErrorWith
+  def handleIOError[A](io: IO[A])(handler: Throwable => A): IO[A] =
+//    io.handleError(handler)
+    io.redeem(handler, identity)
+
+  def handleIOErrorWith[A](io: IO[A])(handler: Throwable => IO[A]): IO[A] =
+//    io.handleErrorWith(handler)
+    io.redeemWith(handler, IO.pure)
+
+  def main(args: Array[String]): Unit = {
+    import cats.effect.unsafe.implicits.global
+//    dealWithIt.unsafeRunSync()
+    resultAsEffect.unsafeRunSync()
+  }
+}
