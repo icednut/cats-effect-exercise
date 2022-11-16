21c21f234640e46d76a9f6b56a2201bcd501e30e
icednut <wglee21g@gmail.com>
2022-11-16 14:12:09 +0900

    Cancellation study

--- src/main/scala/io/icednut/cats/effect/exercise/part3concurrency/CancellingIOs.scala
+++ src/main/scala/io/icednut/cats/effect/exercise/part3concurrency/CancellingIOs.scala
@@ -38,4 +38,67 @@ object CancellingIOs extends IOApp.Simple:
     _ <- fib.join
   } yield ()
 
-  override def run = noCancellationOfDoom
+  /*
+    The uncancelible API is more complex and more general.
+    It takes a function from Poll[IO] to IO. In the example above, we aren't using that Poll instance.
+    The poll object can be used to mark sections within the returned effect which CAN BE CANCELED.
+   */
+
+  /*
+    Example: authentication service. Has two parts:
+    - input password, can be cancelled, because otherwise we might block indefinitely on your input
+    - verify password, CANNOT be cancelled once it's started
+   */
+  val inputPassword: IO[String] = IO("Input password: ").debug >> IO("(typing password)").debug >> IO.sleep(5.seconds) >> IO("RockTheJVM1!")
+  val verifyPassword: String => IO[Boolean] = (pw: String) => IO("verifying...").debug >> IO.sleep(2.second) >> IO(pw == "RockTheJVM1!")
+
+  val authFlow: IO[Unit] = IO.uncancelable { poll =>
+    for {
+      pw <- poll(inputPassword).onCancel(IO("Authentication timed out. Try again later.").debug.void) // this is cancelable
+      verified <- verifyPassword(pw) // this is NOT cancelable
+      _ <- if (verified) IO("Authentication successful.").debug else IO("Authentication failed.").debug // this is NOT cancelable
+    } yield ()
+  }
+
+  val authProgram = for {
+    authFib <- authFlow.start
+    _ <- IO.sleep(3.second) >> IO("Authentication timeout, attempting cancel...").debug >> authFib.cancel
+    _ <- authFib.join
+  } yield ()
+
+  /*
+    Uncancelable calls MASKS which suppress cancellation.
+    Poll calls are "gaps opened" in the uncancelable region.
+   */
+
+  /**
+   * Exercises
+   */
+  // 1
+  val cancelBeforeMol = IO.canceled >> IO(42).debug
+  val uncancelableMol = IO.uncancelable(_ => IO.canceled >> IO(42).debug)
+  // uncancelable will eliminate ALL cancel points
+
+  // 2
+  val invincibleAuthProgram = for {
+    authFib <- IO.uncancelable(_ => authFlow).start
+    _ <- IO.sleep(3.second) >> IO("Authentication timeout, attempting cancel...").debug >> authFib.cancel
+    _ <- authFib.join
+  } yield ()
+
+  // 3
+  def threeStepProgram(): IO[Unit] = {
+    val sequence = IO.uncancelable { poll =>
+      poll(IO("cancelable").debug >> IO.sleep(1.second)) >>
+        IO("uncancelable").debug >> IO.sleep(1.second) >>
+        poll(IO("second cancelable").debug >> IO.sleep(1.second))
+    }
+
+    for {
+      fib <- sequence.start
+      _ <- IO.sleep(1500.millis) >> IO("CANCELING").debug >> fib.cancel
+      _ <- fib.join
+    } yield ()
+  }
+
+  override def run = threeStepProgram()
